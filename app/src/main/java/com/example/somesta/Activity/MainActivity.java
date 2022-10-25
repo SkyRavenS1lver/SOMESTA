4d1e3a96d18ac4b009fe5e2e1076a19aebf09d57
SkyRavenS1lver <101544440+SkyRavenS1lver@users.noreply.github.com>
2022-10-25 10:01:05 +0700

    update search not case sensitive

--- app/src/main/java/com/example/somesta/Activity/MainActivity.java
+++ app/src/main/java/com/example/somesta/Activity/MainActivity.java
@@ -139,7 +139,6 @@ public class MainActivity extends AppCompatActivity{
                 return false;
             }
         });
-
         //Firebase Database Data Grabber
         reference.addValueEventListener(new ValueEventListener() {
             @Override
