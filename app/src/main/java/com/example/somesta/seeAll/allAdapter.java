7a35420cfae10f4e373e794ad381598c6dfcc73a
Andhika_R <andhika.rahmanu@mail.ugm.ac.id>
2022-11-12 14:54:35 +0700

    v1.7 improved filter UI UX

--- app/src/main/java/com/example/somesta/seeAll/allAdapter.java
+++ app/src/main/java/com/example/somesta/seeAll/allAdapter.java
@@ -5,10 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.somesta.Activity.MainActivity;
 import com.example.somesta.R;
 import java.util.HashSet;
 import java.util.List;
@@ -40,9 +44,22 @@ public class allAdapter extends RecyclerView.Adapter<allAdapter.HolderData> {
 
     @Override
     public void onBindViewHolder(@NonNull HolderData holder, int position) {
-        holder.group.setText(listData.get(position));
-        String name = holder.group.getText().toString();
+//        holder.group.setText(listData.get(position));
+        holder.group_text.setText(listData.get(position));
+        String name = holder.group_text.getText().toString();
         holder.group.setChecked(grouped.contains(name));
+        holder.group_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.group.isChecked()){
+                    grouped.remove(name);
+                    holder.group.setChecked(false);
+                }else{
+                    grouped.add(name);
+                    holder.group.setChecked(true);
+                }
+            }
+        });
         holder.group.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,9 +79,13 @@ public class allAdapter extends RecyclerView.Adapter<allAdapter.HolderData> {
 
     public class HolderData extends RecyclerView.ViewHolder{
         public CheckBox group;
+        public TextView group_text;
+        public View group_layout;
         public HolderData(@NonNull View itemView) {
             super(itemView);
             group = itemView.findViewById(R.id.group);
+            group_text = itemView.findViewById(R.id.group_text);
+            group_layout = itemView.findViewById(R.id.group_layout);
         }
     }
 }
